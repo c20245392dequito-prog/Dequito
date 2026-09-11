6e0865d30a215da3575c30b70869de887aa9871e
c20245392dequito-prog <c20245392.dequito@csav.edu.ph>
2026-09-11 09:18:30 +0800

    variable (let & const)

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello World.");
+console.log("Jive Miguel Valencia Dequito BSIS 3A.");
+const Myname= "Ryomen Sukuna";
+
+
+let age=1000;
+let number=9307134230;
+
+const address = "Shibuya";
+
+console.log("Name: " +Myname);
+console.log("age: " +age);
+console.log ("number: " + number);
+console.log("address: " + address)
