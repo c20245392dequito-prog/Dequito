e095ec965c7580354f287feeff48c9476c41206c
c20245392dequito-prog <c20245392.dequito@csav.edu.ph>
2026-09-11 11:38:10 +0800

    TextContext

--- script.js
+++ script.js
@@ -29,13 +29,18 @@ console.log(mdas(5, 3));
 const heading = document.querySelector("h1");
 console.log(heading);
 
-const contactheading = document.querySelector("#contanct h2");
+const contactheading = document.querySelector("#contact h2");
 console.log(contactheading);
 
 const projectheading = document.querySelector("#services h2");
 console.log(projectheading);
 
-heading.textContent = "My portfolio";
-contactheading.textContent = "Let's get in touch";
-projectheading.textContent = "What I can do for you"; 
 
+//Text Context
+
+heading.textContent = "My portfolio";
+contactheading.textContent = "Contact me";
+projectheading.textContent = "My services";
+
+heading.style.color = "purple";
+contactheading.style.color = "blue";
