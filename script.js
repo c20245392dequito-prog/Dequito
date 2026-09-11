de8a126fe2757647e775e0d5520ca777570d21f7
c20245392dequito-prog <c20245392.dequito@csav.edu.ph>
2026-09-11 10:42:45 +0800

    Functions

--- script.js
+++ script.js
@@ -11,3 +11,39 @@ console.log("Name: " +Myname);
 console.log("age: " +age);
 console.log ("number: " + number);
 console.log("address: " + address)
+
+
+function greet(name) {
+    return `Good Morning, ${name}`;
+}
+
+let name="Jive Miguel";
+console.log (greet(name))
+
+
+
+
+
+
+
+function add(a, b) {
+    return a + b;
+    
+}
+
+function sub(a, b) {
+    return a - b;
+}
+
+function mul(a, b) {
+    return a * b;
+}
+
+function div(a, b) {
+    return a / b;
+}
+
+console.log("Add: "+add(5, 3));
+console.log("sub: "+sub(5, 3));
+console.log("mul: "+(5, 3));
+console.log("div: "+div(5, 3));
