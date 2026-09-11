1549d593199f5524bcaa16b7878b9dcf6187b16e
c20245392dequito-prog <c20245392.dequito@csav.edu.ph>
2026-09-11 12:18:34 +0800

    addEventListener

--- script.js
+++ script.js
@@ -45,4 +45,24 @@ contactHeading.style.color = "purple";
 projectHeading.style.color = "purple";
 servicesHeading.style.color = "purple";
 
+heading.addEventListener("click", function () {
+    heading.style.color = "red";
+});
+
+const toggleButton = document.querySelector('#switch');
+const body = document.querySelector('body');
+let isOff = false;
+
+toggleButton.addEventListener("click", function () {
+    isOff = !isOff;
+
+    if (isOff) {
+        body.style.backgroundColor = "black";
+    } else {
+        body.style.backgroundColor = "white";
+
+
+    }
+});
+
 
