f8ebc51679cad72b2a0762f7eaa33cd9af470c83
c20245392dequito-prog <c20245392.dequito@csav.edu.ph>
2026-09-25 09:17:40 +0800

    Change MyProjects bg to orange

--- script.js
+++ script.js
@@ -58,9 +58,10 @@ toggleButton.addEventListener("click", function () {
 
     if (isOff) {
         body.style.backgroundColor = "black";
+        body.style.color = "white";
     } else {
         body.style.backgroundColor = "white";
-
+        body.style.color = "black";
 
     }
 });
